a28ddfebbff6c1621e86d4cabcf008e2b41c1b35
Ironesto <gabriel.p.g@hotmail.es>
2023-05-31 20:40:12 +0200

    commit

--- Piscina0/ft_print_comb.c
+++ Piscina0/ft_print_comb.c
@@ -6,31 +6,32 @@ void ft_print_comb(void)
     char pri = '0';
     char seg = pri + 1;
     char ter = seg + 1;
-    while(ter < '9')
+    while(pri <= '7')
     {
-        write(1,&pri,1);
-        write(1,&seg,1);
-        write(1,&ter,1);
-        write(1," ",1);
-        ter++;
-    }
-     while(seg < '8')
-    {
-        write(1,&pri,1);
-        write(1,&seg,1);
-        write(1,&ter,1);
-        write(1," ",1);
-        seg++;
-    }
-     while(pri <= '7')
-    {
+        while(ter < '9')
+        {
+            write(1,&pri,1);
+            write(1,&seg,1);
+            write(1,&ter,1);
+            write(1," ",1);
+            ter++;
+        }
+        ter =seg + 1;
+        while(seg < '8')
+        {
+            write(1,&pri,1);
+            write(1,&seg,1);
+            write(1,&ter,1);
+            write(1," ",1);
+            seg++;
+        }
+        seg = pri + 1;
         write(1,&pri,1);
         write(1,&seg,1);
         write(1,&ter,1);
         write(1," ",1);
         pri++;
     }
-
 
 }
 
