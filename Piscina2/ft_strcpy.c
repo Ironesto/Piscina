f6b8a79907478154b8c9aa0a0e48b4b6141967ab
Ironesto <gabriel.p.g@hotmail.es>
2023-06-10 20:29:00 +0200

    commit

--- Piscina2/ft_strcpy.c
+++ Piscina2/ft_strcpy.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 char *ft_strcpy(char *dest, char *src)
 {
     int c = 0;
+    dest = malloc(100 * sizeof(char));
     while(*src != '\0')
     {
         dest[c] = src[c];
         c++;
     }
+    return (dest);
+    free;
 }
 
 int main(void)
