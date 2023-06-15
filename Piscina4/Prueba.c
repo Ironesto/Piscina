6e8d2686ed9a40e5d5b8c289958c0078c40e3804
ironesto <gabriel.p.g@hotmail.es>
2023-06-15 19:34:12 +0200

    Prueba

--- Piscina4/Prueba.c
+++ Piscina4/Prueba.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 #include <unistd.h>
 
+char ft_conver(int num)
+{
+    char let = num + 48;
+    write(1,&let,1);
+}
+
 void ft_putnbr(int nb)
 {
+    int nb2 = nb;
+    int control = nb;
     char let;
-    int *nb2 = nb;
-    while(nb > 10)
+    while( nb > 10)
         nb = nb / 10;
     let = nb + 48;
     write(1,&let,1);
-    while (nb < nb2)
-        *nb2 = *nb2 / 10;
-    
-    
+    while(nb < nb2)
+    {
+        nb = nb * 10;
+        while (nb2 > nb * 10)
+            nb2 = nb2 / 10;
+        nb2 = nb2 - nb;
+        nb = nb + nb2;
+        let = nb2 + 48;
+        write(1,&let,1);
+        nb2 = control;
+    }
 }
 
 int main(void)
 {
-    ft_putnbr(34960);
+    ft_putnbr(3496);
 }
+
+
+/*  char let;
+    while(nb >10)
+    {
+        ft_putnbr(nb / 10);
+        nb = nb - ((nb / 10) * 10);
+        let = nb +48;
+        write(1,&let,1);
+    }*/
--- Piscina4/ft_putnbr.c
+++ Piscina4/ft_putnbr.c
@@ -3,14 +3,24 @@
 
 void ft_putnbr(int nb)
 {
+    int nb2 = nb;
+    int control = nb;
     char let;
-    while(nb > 10)
-    {
-    ft_putnbr(nb / 10);
+    while( nb > 10)
+        nb = nb / 10;
     let = nb + 48;
-    write(1,&nb,1);
+    write(1,&let,1);
+    while(nb < nb2)
+    {
+        nb = nb * 10;
+        while (nb2 > nb * 10)
+            nb2 = nb2 / 10;
+        nb2 = nb2 - nb;
+        nb = nb + nb2;
+        let = nb2 + 48;
+        write(1,&let,1);
+        nb2 = control;
     }
-    
 }
 
 int main(void)
