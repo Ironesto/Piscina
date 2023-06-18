053f7b3501f4141835da173175f12ec35626b784
Ironesto <gabriel.p.g@hotmail.es>
2023-06-18 21:01:01 +0200

    Arreglo primos negativos

--- Piscina5/ft_find_next_prime.c
+++ Piscina5/ft_find_next_prime.c
@@ -6,13 +6,13 @@ int	ft_find_next_prime(int nb)
     int n = 2;
     while(n < nb -1 && nb % n != 0)
         n++;
-    if(nb % n != 0 || nb == 2 || nb == 0)
+    if(nb % n > 0 || nb == 2 || nb == 0)
         return(nb);
     return(ft_find_next_prime(nb + 1));
 }
 
 int main(void)
 {
-    printf("Resultado : %d\n",ft_find_next_prime(38));
+    printf("Resultado : %d\n",ft_find_next_prime(4));
     return(0);
 }
--- Piscina5/ft_is_prime.c
+++ Piscina5/ft_is_prime.c
@@ -6,13 +6,13 @@ int	ft_is_prime(int nb)
     int n = 2;
     while(n < nb -1 && nb % n != 0)
         n++;
-    if(nb % n != 0 || nb == 2 || nb == 0)
+    if(nb % n > 0 || nb == 2 || nb == 0)
         return(1);
     return(0);
 }
 
 int main(void)
 {
-    printf("Resultado : %d\n",ft_is_prime(29));
+    printf("Resultado : %d\n",ft_is_prime(-13));
     return(0);
 }
